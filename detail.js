fa8ddebbc06ce7f2aa4f65a6d3eb3c457920592f
shenyuchao <yuchao.shen@visitshanghai.net>
2020-05-26 17:09:30 +0800

    完成

--- detail.js
+++ detail.js
@@ -1,4 +1,8 @@
 $(function() {
+    // 监听页面刷新
+    window.onbeforeunload = function() {
+        return "确认要刷新此页面吗？";
+    };
     // 获取页面复制的数据
     function getPageData(id) {
         let data = document.querySelector('#'+id);
@@ -39,7 +43,7 @@ $(function() {
             ele.specs.forEach(function(subEle) {
                 str += subEle.spec_value + ' ';
             });
-            str += '】';
+            str += `(${ele.groupPrice})】`;
             skusDesArr.push(str);
         });
         return skusDesArr.join('，');
@@ -58,6 +62,7 @@ $(function() {
         table.append(trDom);
     }
 
+    // 页面上的产品数据表格JSON
     let dataList = [];
     let commentList = [];
     // 筛选并存储数据
@@ -214,14 +219,101 @@ $(function() {
         });
     }
 
+    let echartsOptions = [];
     // 评论数据分析
     document.querySelector('#analyze').addEventListener('click', function() {
         if (Object.keys(commentObject).length <= 0) {
             alert('请先录入评论数据');
             return;
         }
-        let echartsOptions = getEchartsData(commentObject);
+        echartsOptions = getEchartsData(commentObject);
         console.log(echartsOptions);
         initEcharts(echartsOptions);
+    });
+
+    /* -----------------------------------导出excel ------------------------------*/
+    /**
+     * 通用的打开下载对话框方法，没有测试过具体兼容性
+     * @param url 下载地址，也可以是一个blob对象，必选
+     * @param saveName 保存文件名，可选
+     */
+    function openDownloadDialog(url, saveName)
+    {
+        if(typeof url == 'object' && url instanceof Blob)
+        {
+            url = URL.createObjectURL(url); // 创建blob地址
+        }
+        var aLink = document.createElement('a');
+        aLink.href = url;
+        aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
+        var event;
+        if(window.MouseEvent) event = new MouseEvent('click');
+        else
+        {
+            event = document.createEvent('MouseEvents');
+            event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        }
+        aLink.dispatchEvent(event);
+    }
+
+    // 将一个sheet转成最终的excel文件的blob对象，然后利用URL.createObjectURL下载
+    function sheet2blob(sheet, sheetName) {
+        sheetName = sheetName || 'sheet1';
+        var workbook = {
+            SheetNames: [sheetName],
+            Sheets: {}
+        };
+        workbook.Sheets[sheetName] = sheet;
+        // 生成excel的配置项
+        var wopts = {
+            bookType: 'xlsx', // 要生成的文件类型
+            bookSST: false, // 是否生成Shared String Table，官方解释是，如果开启生成速度会下降，但在低版本IOS设备上有更好的兼容性
+            type: 'binary'
+        };
+        var wbout = XLSX.write(workbook, wopts);
+        var blob = new Blob([s2ab(wbout)], {type:"application/octet-stream"});
+        // 字符串转ArrayBuffer
+        function s2ab(s) {
+            var buf = new ArrayBuffer(s.length);
+            var view = new Uint8Array(buf);
+            for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+            return buf;
+        }
+        return blob;
+    }
+
+    // 导出excel
+    function exportExcel(data) {
+        var sheet = XLSX.utils.json_to_sheet(data);
+        openDownloadDialog(sheet2blob(sheet), '导出.xlsx');
+    }
+
+    // 评论导出数据整理
+    function commentFileData(commentObject) {
+        let arr = [];
+        const obj = {};
+        for (const key in commentObject) {
+            if (commentObject.hasOwnProperty(key)) {
+                const eleArr = commentObject[key];
+                eleArr.forEach(function (subEle) {
+                    let specStr = getSkuSpec(subEle.specs);
+                    if (obj[specStr]) {
+                        obj[specStr]++;
+                    } else {
+                        obj[specStr] = 1;
+                    }
+                })
+                arr.push([key].concat(Object.keys(obj)));
+                arr.push(['数量'].concat(Object.values(obj)));
+            }
+        }
+        return arr;
+    }
+
+    document.querySelector('#export').addEventListener('click', function() {
+        // 导出产品列表
+        exportExcel(dataList);
+        // 导出评论列表
+        exportExcel(commentFileData(commentObject));
     });
 });
